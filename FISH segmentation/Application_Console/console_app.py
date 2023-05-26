b766ff6e3d1dca43760f634d13752cb9ebbfd634
Alexander Trykin <thesanchouz@gmail.com>
2023-05-26 19:52:05 +0300

    add chromosome detection

--- FISH segmentation/Application_Console/console_app.py
+++ FISH segmentation/Application_Console/console_app.py
@@ -2,22 +2,29 @@ import argparse
 import logging as log
 import os
 
-from io_utils import read_czi_file_as_pil_image
-from ultralytics import YOLO
+from matplotlib import pyplot as plt
+
+from chromosome_detection import ChromosomeCellDetector
+from io_utils import read_czi_image
+
 
 def cli_argument_parser():
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('-i', '--input',
-                        type=str,
-                        dest='input',
-                        help='Path to input image (.czi .png .jpg)',
-                        required=True)
-    parser.add_argument('-t', '--threshold',
-                        dest='threshold',
-                        type=float,
-                        help='Threshold for object prediction',
-                        default=0.5)
+    parser.add_argument(
+        '-i', '--input',
+        type=str,
+        dest='input',
+        help='Path to input image (.czi .png .jpg)',
+        required=True
+    )
+    parser.add_argument(
+        '-t', '--threshold',
+        dest='threshold',
+        type=float,
+        help='Threshold for object prediction',
+        default=0.5
+    )
 
     args = parser.parse_args()
 
@@ -28,24 +35,17 @@ if __name__ == '__main__':
     log.basicConfig(format='[%(levelname)s]:%(message)s', level=log.INFO)
     args = cli_argument_parser()
 
-    log.info(f'Load Segmentation model')
-    FileModelPath = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..\\Model\\my_yolov8_model_core_segmentation.pt")
-    model = YOLO(FileModelPath)
-
     log.info(f'Read input image {args.input}')
-    image = read_czi_file_as_pil_image(args.input)
-    
+    image, _ = read_czi_image(args.input)
+
+    detector = ChromosomeCellDetector(image)
     log.info(f'Perform segmentation')
-    predictions = model.predict(
-        image, 
-        show = False, 
-        classes=[0,1], 
-        save = True,
-        project='..\\Photo_Console',
-        name=f'{os.path.basename(args.input)}',
-        show_labels = False,
-        show_conf = False,
-        save_txt = True,
-        stream=False,
-        conf=args.threshold,
-        line_thickness=1)
+    detector.find_cells(confidence=0.5)
+    log.info(f'Perform chromosomes detection')
+    detector.detect_chromosomes()
+
+    fig, ax = plt.subplots(1, 1, figsize=(16, 16), dpi=300)
+    ax = detector.plot(ax)
+
+    fname = f'..\\Photo_Console\\{os.path.basename(args.input).split(".")[0]}.png'
+    fig.savefig(fname, dpi='figure', format='png')
