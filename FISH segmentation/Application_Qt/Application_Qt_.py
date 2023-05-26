998eccb5c07a14db92c0f3bc3cd224a774d1dcd3
Данила Ермолаев <73434195+Dnoniel-Ermolaev@users.noreply.github.com>
2023-05-26 17:48:02 +0300

    Fix some issues
    -New requirements.txt files
    -Add showing speed of prediction
    -Formatt all applications, delete not usable import's

--- FISH segmentation/Application_Qt/Application_Qt_.py
+++ FISH segmentation/Application_Qt/Application_Qt_.py
@@ -1,27 +1,28 @@
-from PyQt6.QtWidgets import (QApplication, QWidget, QPushButton ,QTextEdit, QComboBox, QFileDialog, QHBoxLayout, QVBoxLayout)
-from PyQt6.QtGui import QImage, QPixmap
-from PyQt6.QtCore import QFileInfo, Qt
-#import matplotlib.pyplot as plt
-#from roboflow import Roboflow
-from ultralytics import YOLO
-from PIL import Image
-import numpy as np
-import czifile
-#import sys
-import os
 import io
-#from PyQt6.QtCore import Qt
-#import cv2
+import os
 import shutil
-from PyQt6 import QtCore, QtGui, QtWidgets
+
+import czifile
+import numpy as np
+from PIL import Image
+from PyQt6 import QtWidgets
+from PyQt6.QtCore import Qt
+from PyQt6.QtGui import QImage, QPixmap
+from PyQt6.QtWidgets import QFileDialog
+from ultralytics import YOLO
 
 from Application_Qt_module_ui import Ui_MainWindow
 
+
 class Func(Ui_MainWindow):
-    FILEPATH = '-1'
-    FILENAME = '-1'
-    FileModelPath = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..\\Model\\my_yolov8_model_core_segmentation.pt")
+    FILEPATH = "-1"
+    FILENAME = "-1"
+    FileModelPath = os.path.join(
+        os.path.dirname(os.path.abspath(__file__)),
+        "..\\Model\\my_yolov8_model_core_segmentation.pt",
+    )
     model = YOLO(FileModelPath)
+
     def setupUiFunc(self, MainWindow):
         self.setupUi(MainWindow)
         self.add_functions()
@@ -31,33 +32,40 @@ class Func(Ui_MainWindow):
         self.pushButtonStart.clicked.connect(self.add_image)
         self.pushButtonSeg.clicked.connect(self.predict_image)
 
+    # Основная фун-я по сегментации:
+    def predict_image(self):  # ИСПРАВИТЬ: не допускать точности, ниже 0.2;
+        if Func.FILEPATH != "-1":  # Значит, что картинка была сохранена, путь есть;
+            Accuracy = self.labelAccuracy.text()  # Берем точность с поля;
+            # FileModelPath = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..\\Model\\my_yolov8_model_core_segmentation.pt")
 
-# Основная фун-я по сегментации:
-    def predict_image(self):                     # ИСПРАВИТЬ: не допускать точности, ниже 0.2;
-        if Func.FILEPATH != '-1':       # Значит, что картинка была сохранена, путь есть;
-            Accuracy = self.labelAccuracy.text() # Берем точность с поля;
-            #FileModelPath = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..\\Model\\my_yolov8_model_core_segmentation.pt")
-
-            #model = YOLO(FileModelPath)
+            # model = YOLO(FileModelPath)
 
             img = Image.open(Func.FILEPATH)
 
-            folder_path = '..\\Photo_Qt\\' + Func.FILENAME  # Имя папки. If ниже проверяет, существует ли такая папка, есла да - удалить.
-            if os.path.exists(folder_path):                          # Фун-я model.predict не может сама перезаписать папку с полученным изображением;
+            folder_path = (
+                "..\\Photo_Qt\\" + Func.FILENAME
+            )  # Имя папки. If ниже проверяет, существует ли такая папка, есла да - удалить.
+            if os.path.exists(
+                folder_path
+            ):  # Фун-я model.predict не может сама перезаписать папку с полученным изображением;
                 shutil.rmtree(folder_path)
 
-            predictions = Func.model.predict(img, 
-                                        show = False, # Не показывать результат дополнительным окном;
-                                        classes=[0,1],# Есть два класса;
-                                        save = True,  # Сохранять полученное фото;
-                                        project='..\\Photo_Qt',          # Папка сохранения;
-                                        name=Func.FILENAME,  # model.predict сначала создасть папку с именем Ui_MainWindow.FILENAME (внутри папки project = ' '), только затем положит туда изображнеие с таким же названием, как и img; 
-                                        show_labels = False, 
-                                        show_conf = False,
-                                        save_txt = True,      # Сохранит рядом с обработанным изображением в папку Ui_MainWindow.FILENAME;
-                                        conf=float(Accuracy), # Точность;
-                                        line_thickness=1) # Толщина границ выделения;
-            Path = '..\\Photo_Qt\\' + Func.FILENAME + '\\' + Func.FILENAME + '.jpg' # Берем путь, чтобы вывести на экран;
+            predictions = Func.model.predict(
+                img,
+                show=False,  # Не показывать результат дополнительным окном;
+                classes=[0, 1],  # Есть два класса;
+                save=True,  # Сохранять полученное фото;
+                project="..\\Photo_Qt",  # Папка сохранения;
+                name=Func.FILENAME,  # model.predict сначала создасть папку с именем Ui_MainWindow.FILENAME (внутри папки project = ' '), только затем положит туда изображнеие с таким же названием, как и img;
+                show_labels=False,
+                show_conf=False,
+                save_txt=True,  # Сохранит рядом с обработанным изображением в папку Ui_MainWindow.FILENAME;
+                conf=float(Accuracy),  # Точность;
+                line_thickness=1,
+            )  # Толщина границ выделения;
+            Path = (
+                "..\\Photo_Qt\\" + Func.FILENAME + "\\" + Func.FILENAME + ".jpg"
+            )  # Берем путь, чтобы вывести на экран;
             pixmap = QPixmap(Path)
             self.label_2.setPixmap(pixmap)
             # Доп.Информация:
@@ -66,29 +74,41 @@ class Func(Ui_MainWindow):
             number_explode = 0
             for r in predictions:
                 for c in r.boxes.cls:
-                    if (names[int(c)]=="Whole cell"): number_whole+=1
-                    if (names[int(c)]=="Explode cell"): number_explode+=1
+                    if names[int(c)] == "Whole cell":
+                        number_whole += 1
+                    if names[int(c)] == "Explode cell":
+                        number_explode += 1
             print("Explode:")
             print(number_explode)
             print("Whole:")
             print(number_whole)
-            ref = "Whole cell: " + str(number_whole) + "\nExplode cell: " + str(number_explode)
+            ref = (
+                "Whole cell: "
+                + str(number_whole)
+                + "\nExplode cell: "
+                + str(number_explode)
+            )
             self.Reference.setText(ref)
 
-        
-# фун-я, сохраняющая картинку:
+    # фун-я, сохраняющая картинку:
     def PhotoSave(self, pixmap, filename):
-        folder_name = "..\\Photo_Qt"          # Имя папки;
+        folder_name = "..\\Photo_Qt"  # Имя папки;
 
-        if not os.path.exists(folder_name):   # Создать папку, если ее нет; 
+        if not os.path.exists(folder_name):  # Создать папку, если ее нет;
             os.makedirs(folder_name)
 
-        Image_name = os.path.splitext(os.path.basename(filename))[0] # Для получения имени файла без расширения и пути к нему;
+        Image_name = os.path.splitext(os.path.basename(filename))[
+            0
+        ]  # Для получения имени файла без расширения и пути к нему;
 
-        dir_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..\\Photo_Qt")
-        jpg = '.jpg'
+        dir_path = os.path.join(
+            os.path.dirname(os.path.abspath(__file__)), "..\\Photo_Qt"
+        )
+        jpg = ".jpg"
         # print (dir_path)
-        filepath = os.path.join(dir_path, Image_name) # Объединяет путь к директории (dir_path) и имя файла (Image_name), чтобы создать полный путь к файлу;
+        filepath = os.path.join(
+            dir_path, Image_name
+        )  # Объединяет путь к директории (dir_path) и имя файла (Image_name), чтобы создать полный путь к файлу;
         filepath = filepath + jpg
         # print (filepath)
         Func.FILEPATH = filepath
@@ -96,10 +116,9 @@ class Func(Ui_MainWindow):
         # print (Ui_MainWindow.FILENAME)
         pixmap.save(filepath)
 
-
-# Обработка изображения, если оно передано в формате .czi:
+    # Обработка изображения, если оно передано в формате .czi:
     def process_image(self, filename):
-        if filename.endswith('.czi'):
+        if filename.endswith(".czi"):
             # Откроем ZIP-файл с помощью czifile:
             with czifile.CziFile(os.path.join(filename)) as czi:
                 # Получить все каналы в файле:
@@ -113,38 +132,42 @@ class Func(Ui_MainWindow):
                 img = img.resize((450, 450))
                 # Преобразуем из Pillow в qimage через BytesIO:
                 byte_array = io.BytesIO()
-                img.save(byte_array, format='PNG')
+                img.save(byte_array, format="PNG")
                 byte_array = byte_array.getvalue()
                 qimage = QImage.fromData(byte_array)
                 pixmap = QPixmap.fromImage(qimage)
                 self.labelFoto.setPixmap(pixmap)
                 # Создадим папку и положим туда выбранное изображение:
-                self.PhotoSave(pixmap,filename)
+                self.PhotoSave(pixmap, filename)
 
-
-# Выбор изображения и добавление его в окно для исходного изображения:
+    # Выбор изображения и добавление его в окно для исходного изображения:
     def add_image(self):
-        filename, _ = QFileDialog.getOpenFileName(None, "Open Image", os.getcwd(), "Images (*.png *.jpg *.czi)")
-        
+        filename, _ = QFileDialog.getOpenFileName(
+            None, "Open Image", os.getcwd(), "Images (*.png *.jpg *.czi)"
+        )
+
         if filename:
-             if filename.endswith('.jpg'):
-                 window_size = self.labelFoto.size()
-                 pixmap = QPixmap(filename).scaled(450, 450, Qt.AspectRatioMode.KeepAspectRatio) #window_size.width(), window_size.height(), Qt.AspectRatioMode.KeepAspectRatio
-                 self.labelFoto.setPixmap(pixmap)
-                 self.PhotoSave(pixmap,filename)
+            if filename.endswith(".jpg"):
+                pixmap = QPixmap(filename).scaled(
+                    450, 450, Qt.AspectRatioMode.KeepAspectRatio
+                )  # window_size.width(), window_size.height(), Qt.AspectRatioMode.KeepAspectRatio
+                self.labelFoto.setPixmap(pixmap)
+                self.PhotoSave(pixmap, filename)
 
-             if filename.endswith('.png'):
-                 window_size = self.labelFoto.size()
-                 pixmap = QPixmap(filename).scaled(450, 450, Qt.AspectRatioMode.KeepAspectRatio) #window_size.width(), window_size.height(), Qt.AspectRatioMode.KeepAspectRatio
-                 self.labelFoto.setPixmap(pixmap)
-                 self.PhotoSave(pixmap,filename)
+            if filename.endswith(".png"):
+                pixmap = QPixmap(filename).scaled(
+                    450, 450, Qt.AspectRatioMode.KeepAspectRatio
+                )  # window_size.width(), window_size.height(), Qt.AspectRatioMode.KeepAspectRatio
+                self.labelFoto.setPixmap(pixmap)
+                self.PhotoSave(pixmap, filename)
 
-             if filename.endswith('.czi'):
-                 self.process_image(filename)
+            if filename.endswith(".czi"):
+                self.process_image(filename)
 
 
 if __name__ == "__main__":
     import sys
+
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
     ui = Func()
--- FISH segmentation/Application_Qt/Application_Qt_module_ui.py
+++ FISH segmentation/Application_Qt/Application_Qt_module_ui.py
@@ -15,79 +15,89 @@ class Ui_MainWindow(object):
         MainWindow.resize(918, 684)
         MainWindow.setMaximumSize(QtCore.QSize(16777215, 16777215))
         icon = QtGui.QIcon()
-        icon.addPixmap(QtGui.QPixmap("resources/icon.ico"), QtGui.QIcon.Mode.Normal, QtGui.QIcon.State.Off)
+        icon.addPixmap(
+            QtGui.QPixmap("resources/icon.ico"),
+            QtGui.QIcon.Mode.Normal,
+            QtGui.QIcon.State.Off,
+        )
         MainWindow.setWindowIcon(icon)
         MainWindow.setAutoFillBackground(False)
-        MainWindow.setStyleSheet("#MainWindow\n"
-"{\n"
-"border-image: url(resources/Background4.jpg);\n"
-"background-position: center center no-repeat  fixed;\n"
-"background-size: 100% 100%;\n"
-"\n"
-"}\n"
-"\n"
-"\n"
-"\n"
-"\n"
-"")
+        MainWindow.setStyleSheet(
+            "#MainWindow\n"
+            "{\n"
+            "border-image: url(resources/Background4.jpg);\n"
+            "background-position: center center no-repeat  fixed;\n"
+            "background-size: 100% 100%;\n"
+            "\n"
+            "}\n"
+            "\n"
+            "\n"
+            "\n"
+            "\n"
+            ""
+        )
         MainWindow.setAnimated(True)
         MainWindow.setDocumentMode(False)
         MainWindow.setDockNestingEnabled(False)
         MainWindow.setUnifiedTitleAndToolBarOnMac(False)
         self.centralwidget = QtWidgets.QWidget(parent=MainWindow)
-        self.centralwidget.setStyleSheet("#centralwidget {background-color: transparent;}")
+        self.centralwidget.setStyleSheet(
+            "#centralwidget {background-color: transparent;}"
+        )
         self.centralwidget.setObjectName("centralwidget")
         self.verticalLayout = QtWidgets.QVBoxLayout(self.centralwidget)
         self.verticalLayout.setObjectName("verticalLayout")
         self.verticalGroupBox = QtWidgets.QGroupBox(parent=self.centralwidget)
         self.verticalGroupBox.setAcceptDrops(False)
         self.verticalGroupBox.setAutoFillBackground(False)
-        self.verticalGroupBox.setStyleSheet("#verticalGroupBox {\n"
-"border: 1px inset #10f04f;\n"
-"border-radius: 10px;\n"
-"}")
+        self.verticalGroupBox.setStyleSheet(
+            "#verticalGroupBox {\n"
+            "border: 1px inset #10f04f;\n"
+            "border-radius: 10px;\n"
+            "}"
+        )
         self.verticalGroupBox.setFlat(False)
         self.verticalGroupBox.setCheckable(False)
         self.verticalGroupBox.setObjectName("verticalGroupBox")
         self.vboxlayout = QtWidgets.QVBoxLayout(self.verticalGroupBox)
         self.vboxlayout.setObjectName("vboxlayout")
         self.horizontalWidget = QtWidgets.QWidget(parent=self.verticalGroupBox)
-        self.horizontalWidget.setContextMenuPolicy(QtCore.Qt.ContextMenuPolicy.NoContextMenu)
-        self.horizontalWidget.setStyleSheet("#horizontalGroupBox {\n"
-"border: 1px inset #10f04f;\n"
-"}")
+        self.horizontalWidget.setContextMenuPolicy(
+            QtCore.Qt.ContextMenuPolicy.NoContextMenu
+        )
+        self.horizontalWidget.setStyleSheet(
+            "#horizontalGroupBox {\n" "border: 1px inset #10f04f;\n" "}"
+        )
         self.horizontalWidget.setObjectName("horizontalWidget")
         self.horizontalLayout = QtWidgets.QHBoxLayout(self.horizontalWidget)
         self.horizontalLayout.setContentsMargins(5, 0, 5, 0)
         self.horizontalLayout.setSpacing(10)
         self.horizontalLayout.setObjectName("horizontalLayout")
         self.labelFoto = QtWidgets.QLabel(parent=self.horizontalWidget)
-        self.labelFoto.setStyleSheet("#labelFoto\n"
-"{\n"
-"border: 4px inset #10f0ad;\n"
-"\n"
-"}")
+        self.labelFoto.setStyleSheet(
+            "#labelFoto\n" "{\n" "border: 4px inset #10f0ad;\n" "\n" "}"
+        )
         self.labelFoto.setFrameShape(QtWidgets.QFrame.Shape.Box)
         self.labelFoto.setObjectName("labelFoto")
         self.horizontalLayout.addWidget(self.labelFoto)
         self.label_2 = QtWidgets.QLabel(parent=self.horizontalWidget)
-        self.label_2.setStyleSheet("#label_2\n"
-"{\n"
-"border: 4px outset #10f0e9;\n"
-"\n"
-"}")
+        self.label_2.setStyleSheet(
+            "#label_2\n" "{\n" "border: 4px outset #10f0e9;\n" "\n" "}"
+        )
         self.label_2.setFrameShape(QtWidgets.QFrame.Shape.Box)
         self.label_2.setObjectName("label_2")
         self.horizontalLayout.addWidget(self.label_2)
         self.vboxlayout.addWidget(self.horizontalWidget)
         self.gridGroupBox = QtWidgets.QGroupBox(parent=self.verticalGroupBox)
         self.gridGroupBox.setMaximumSize(QtCore.QSize(16777215, 150))
-        self.gridGroupBox.setStyleSheet("#gridGroupBox {\n"
-"                width: 100px;\n"
-"                height:100px;\n"
-"                border: 3px solid #1071f0;\n"
-"                border-radius: 15px;\n"
-"}")
+        self.gridGroupBox.setStyleSheet(
+            "#gridGroupBox {\n"
+            "                width: 100px;\n"
+            "                height:100px;\n"
+            "                border: 3px solid #1071f0;\n"
+            "                border-radius: 15px;\n"
+            "}"
+        )
         self.gridGroupBox.setObjectName("gridGroupBox")
         self.gridLayout = QtWidgets.QGridLayout(self.gridGroupBox)
         self.gridLayout.setContentsMargins(9, 5, 9, 5)
@@ -95,29 +105,35 @@ class Ui_MainWindow(object):
         self.gridLayout.setVerticalSpacing(7)
         self.gridLayout.setObjectName("gridLayout")
         self.pushButtonSeg = QtWidgets.QPushButton(parent=self.gridGroupBox)
-        self.pushButtonSeg.setStyleSheet("QPushButton {\n"
-"  background-color: #ccaa2d;\n"
-"  color: white; \n"
-"  border: 1px solid gray;\n"
-"  padding: 15px;\n"
-"  border-radius: 10px;\n"
-"}\n"
-"\n"
-"QPushButton:pressed {\n"
-"    background-color: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1,\n"
-"                                      stop: 0 #FF7832, stop: 1 #FF9739);\n"
-"}")
+        self.pushButtonSeg.setStyleSheet(
+            "QPushButton {\n"
+            "  background-color: #ccaa2d;\n"
+            "  color: white; \n"
+            "  border: 1px solid gray;\n"
+            "  padding: 15px;\n"
+            "  border-radius: 10px;\n"
+            "}\n"
+            "\n"
+            "QPushButton:pressed {\n"
+            "    background-color: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1,\n"
+            "                                      stop: 0 #FF7832, stop: 1 #FF9739);\n"
+            "}"
+        )
         self.pushButtonSeg.setObjectName("pushButtonSeg")
         self.gridLayout.addWidget(self.pushButtonSeg, 1, 0, 1, 1)
         self.horizontalWidget_2 = QtWidgets.QWidget(parent=self.gridGroupBox)
         self.horizontalWidget_2.setMinimumSize(QtCore.QSize(0, 0))
         self.horizontalWidget_2.setMaximumSize(QtCore.QSize(200, 16777215))
-        self.horizontalWidget_2.setContextMenuPolicy(QtCore.Qt.ContextMenuPolicy.PreventContextMenu)
-        self.horizontalWidget_2.setLayoutDirection(QtCore.Qt.LayoutDirection.LeftToRight)
+        self.horizontalWidget_2.setContextMenuPolicy(
+            QtCore.Qt.ContextMenuPolicy.PreventContextMenu
+        )
+        self.horizontalWidget_2.setLayoutDirection(
+            QtCore.Qt.LayoutDirection.LeftToRight
+        )
         self.horizontalWidget_2.setAutoFillBackground(False)
-        self.horizontalWidget_2.setStyleSheet("#horizontalGroupBox_2{           \n"
-"outline: none;\n"
-"}")
+        self.horizontalWidget_2.setStyleSheet(
+            "#horizontalGroupBox_2{           \n" "outline: none;\n" "}"
+        )
         self.horizontalWidget_2.setObjectName("horizontalWidget_2")
         self.horizontalLayout_2 = QtWidgets.QHBoxLayout(self.horizontalWidget_2)
         self.horizontalLayout_2.setContentsMargins(0, 0, 0, 0)
@@ -134,18 +150,20 @@ class Ui_MainWindow(object):
         self.gridLayout.addWidget(self.horizontalWidget_2, 3, 0, 1, 1)
         self.pushButtonStart = QtWidgets.QPushButton(parent=self.gridGroupBox)
         self.pushButtonStart.setMinimumSize(QtCore.QSize(250, 0))
-        self.pushButtonStart.setStyleSheet("QPushButton {\n"
-"  background-color: #ccaa2d;\n"
-"  color: white; \n"
-"  border: 1px solid gray;\n"
-"  padding: 15px;\n"
-"  border-radius: 10px;\n"
-"}\n"
-"\n"
-"QPushButton:pressed {\n"
-"    background-color: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1,\n"
-"                                      stop: 0 #FF7832, stop: 1 #FF9739);\n"
-"}")
+        self.pushButtonStart.setStyleSheet(
+            "QPushButton {\n"
+            "  background-color: #ccaa2d;\n"
+            "  color: white; \n"
+            "  border: 1px solid gray;\n"
+            "  padding: 15px;\n"
+            "  border-radius: 10px;\n"
+            "}\n"
+            "\n"
+            "QPushButton:pressed {\n"
+            "    background-color: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1,\n"
+            "                                      stop: 0 #FF7832, stop: 1 #FF9739);\n"
+            "}"
+        )
         self.pushButtonStart.setObjectName("pushButtonStart")
         self.gridLayout.addWidget(self.pushButtonStart, 0, 0, 1, 1)
         self.verticalGroupBox_2 = QtWidgets.QGroupBox(parent=self.gridGroupBox)
@@ -153,10 +171,12 @@ class Ui_MainWindow(object):
         self.verticalGroupBox_2.setTabletTracking(False)
         self.verticalGroupBox_2.setAcceptDrops(False)
         self.verticalGroupBox_2.setAutoFillBackground(False)
-        self.verticalGroupBox_2.setStyleSheet("#verticalGroupBox_2 {\n"
-"border: 1px inset #1071f0;\n"
-"border-radius: 10px;\n"
-"}")
+        self.verticalGroupBox_2.setStyleSheet(
+            "#verticalGroupBox_2 {\n"
+            "border: 1px inset #1071f0;\n"
+            "border-radius: 10px;\n"
+            "}"
+        )
         self.verticalGroupBox_2.setTitle("")
         self.verticalGroupBox_2.setFlat(False)
         self.verticalGroupBox_2.setCheckable(False)
@@ -183,7 +203,11 @@ class Ui_MainWindow(object):
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "FISH data processing"))
-        self.labelFoto.setText(_translate("MainWindow", "Исходное изображение, в формате .jpeg, png или .czi"))
+        self.labelFoto.setText(
+            _translate(
+                "MainWindow", "Исходное изображение, в формате .jpeg, png или .czi"
+            )
+        )
         self.label_2.setText(_translate("MainWindow", "Результат сегментации"))
         self.pushButtonSeg.setText(_translate("MainWindow", "Cегментировать"))
         self.labelStart.setText(_translate("MainWindow", "Порог точности:  "))
