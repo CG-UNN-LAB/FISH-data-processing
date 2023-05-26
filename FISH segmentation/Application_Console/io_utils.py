f62f87260de295ce323df93436a1bb5278a4ceaa
Vasiliev Evgenii <eugene.unn@gmail.com>
2023-05-27 01:49:20 +0300

    Add chromosome detection (only console ver) (#13) (#14)
    * add chromosome detection
    * fix confidence
    * add chromosome detection file
    
    Co-authored-by: Alexander Trykin <39907024+TheSanchouz@users.noreply.github.com>

--- FISH segmentation/Application_Console/io_utils.py
+++ FISH segmentation/Application_Console/io_utils.py
@@ -1,18 +1,19 @@
-import os
-
 import czifile
 import numpy as np
-from PIL import Image
 
 
-def read_czi_file_as_pil_image(filename):
-    with czifile.CziFile(os.path.join(filename)) as czi:
-        # Получить все каналы в файле:
-        channels = czi.asarray()[0, 0, :, :, :]
-        # Объедините все каналы в единое RGB-изображение:
-        image_array = np.stack([channels[1], channels[2], channels[0]], axis=-1)
-        # Преобразуем изображение в Pillow:
-        image_array = np.squeeze(image_array)
-        image = np.uint8(image_array)
-        result = Image.fromarray(image)
-    return result
+def read_czi_image(filename, norm=True):
+    with czifile.CziFile(filename) as czi:
+        image = czi.asarray().squeeze()
+        # image = np.moveaxis(image, 0, -1)[..., ::-1]  # swap channels to order -> RGB
+        image = np.stack([image[1], image[2], image[0]], axis=-1)  # swap channels to order -> RGB
+
+        if norm:
+            info = np.iinfo(image.dtype)
+            image = image.astype(np.float64) / info.max  # normalize the data to 0-1
+            image = (255 * image).astype(np.uint8)  # normalize the data to 0-255
+
+        image = np.ascontiguousarray(image)
+        metadata = czi.metadata(raw=True)
+
+        return image, metadata
